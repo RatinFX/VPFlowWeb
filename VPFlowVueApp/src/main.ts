d6a043bdf85b8688f7d909089d20b685cac15257
ratinfx <ratina0@gmail.com>
2025-10-04 14:17:32 +0200

    feat: add icon packages

--- VPFlowVueApp/src/main.ts
+++ VPFlowVueApp/src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from "vue";
 import "./index.css";
 import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 
-createApp(App).mount("#app");
+library.add(fas, far, fab);
+
+const app = createApp(App);
+app.component("fa-icon", FontAwesomeIcon);
+app.mount("#app");
