edb92707dd0cb9df1e26f1894b9ce0695818a66f
ratinfx <ratina0@gmail.com>
2025-10-04 11:11:35 +0200

    refactor: separate messaging

--- VPFlowWebMain/MainForm.cs
+++ VPFlowWebMain/MainForm.cs
@@ -123,35 +123,11 @@ namespace VPFlowWebMain
             }
         }
 
-        internal WebMessage<T> ProcessMessage<T>(string messageJson)
-            where T : BasePayload
-        {
-            Logging.Log("Original messageJson: " + messageJson);
-
-            var ogWebMessage = JsonConvert.DeserializeObject<WebMessage>(messageJson);
-            if (ogWebMessage == null)
-            {
-                //MessageBox.Show("Web message handling error: invalid message format\n- " + messageJson);
-                Logging.Error("Web message handling error: invalid message format: " + messageJson);
-                return null;
-            }
-
-            var senderConvertionSuccess = Enum.TryParse(ogWebMessage.Sender, true, out SenderType sender);
-            if (!senderConvertionSuccess)
-            {
-                //MessageBox.Show("Web message handling error: unknown sender\n- " + ogWebMessage.Sender);
-                Logging.Error("Web message handling error: unknown sender: " + ogWebMessage.Sender);
-                return null;
-            }
-
-            return new WebMessage<T>(sender, ogWebMessage.Payload);
-        }
-
         private void WebVPFlow_WebMessageReceived(object sender, CoreWebView2WebMessageReceivedEventArgs e)
         {
             try
             {
-                var webMessage = ProcessMessage<ApplyPayload>(e.WebMessageAsJson);
+                var webMessage = Messaging.ProcessMessage<ApplyPayload>(e.WebMessageAsJson);
                 Logging.Log($"Processed message by {webMessage.Sender}: " + JsonConvert.SerializeObject(webMessage));
 
                 // TODO: handle message
---
+++ VPFlowWebMain/Messaging.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace VPFlowWebMain
+{
+    internal class Messaging
+    {
+        static internal WebMessage<T> ProcessMessage<T>(string messageJson)
+            where T : BasePayload
+        {
+            Logging.Log("Original messageJson: " + messageJson);
+
+            var ogWebMessage = JsonConvert.DeserializeObject<WebMessage>(messageJson);
+            if (ogWebMessage == null)
+            {
+                //MessageBox.Show("Web message handling error: invalid message format\n- " + messageJson);
+                Logging.Error("Web message handling error: invalid message format: " + messageJson);
+                return null;
+            }
+
+            var senderConvertionSuccess = Enum.TryParse(ogWebMessage.Sender, true, out SenderType sender);
+            if (!senderConvertionSuccess)
+            {
+                //MessageBox.Show("Web message handling error: unknown sender\n- " + ogWebMessage.Sender);
+                Logging.Error("Web message handling error: unknown sender: " + ogWebMessage.Sender);
+                return null;
+            }
+
+            return new WebMessage<T>(sender, ogWebMessage.Payload);
+        }
+    }
+}
