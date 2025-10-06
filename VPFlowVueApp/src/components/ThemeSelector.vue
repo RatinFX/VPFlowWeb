59ee463184ef710bd54f8580d194a0333c015fd8
ratinfx <ratina0@gmail.com>
2025-10-07 01:08:51 +0200

    refactor: theme selector to simple clickable icon

--- VPFlowVueApp/src/components/ThemeSelector.vue
+++ VPFlowVueApp/src/components/ThemeSelector.vue
@@ -4,20 +4,19 @@ import {
   MenubarContent,
   MenubarMenu,
   MenubarTrigger,
-  MenubarRadioGroup,
-  MenubarRadioItem,
 } from "@/components/ui/menubar";
 import store from "@/store";
 import messaging, { MessageType } from "@/lib/messaging";
 
 function onThemeUpdate() {
+  store.theme.value = store.theme.value === "light" ? "dark" : "light";
   messaging.sendMessage(MessageType.Settings);
 }
 </script>
 
 <template>
   <MenubarMenu>
-    <MenubarTrigger>
+    <MenubarTrigger @click="onThemeUpdate">
       <Icon
         icon="radix-icons:sun"
         class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
@@ -28,14 +27,6 @@ function onThemeUpdate() {
       />
       <span class="sr-only">Toggle theme</span>
     </MenubarTrigger>
-    <MenubarContent class="min-w-26">
-      <MenubarRadioGroup
-        v-model="store.theme.value"
-        @update:model-value="onThemeUpdate"
-      >
-        <MenubarRadioItem value="light"> Light </MenubarRadioItem>
-        <MenubarRadioItem value="dark"> Dark </MenubarRadioItem>
-      </MenubarRadioGroup>
-    </MenubarContent>
+    <MenubarContent class="hidden"></MenubarContent>
   </MenubarMenu>
 </template>
